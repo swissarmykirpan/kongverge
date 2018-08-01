8cce0f150fa89cbe991655277412471f64562a08
Tim van Wijk <tim@timvw.co.uk>
2018-08-01 14:51:05 +0100

    Add je-udp-log plugin (#50)
    * Add je-udp-log plugin
    
    * Fix class name

---
+++ src/Kongverge.Common.Tests/JeUdpLogPluginTests.cs
@@ -0,0 +1,41 @@
+using FluentAssertions;
+using Kongverge.Common.Plugins.Custom;
+using Xunit;
+
+namespace Kongverge.Common.Tests
+{
+    public class JeUdpLogPluginTests
+    {
+        [Fact]
+        public void TestRoundTripStartingAtConfigWithNoFields()
+        {
+            var configIn = new JeUdpLogConfig();
+            var plugin = new JeUdpLogPlugin();
+            var configOut = PluginHelpers.RoundTripFromConfig(plugin, configIn);
+            configOut.IsExactMatch(configIn).Should().BeTrue();
+        }
+
+        [Fact]
+        public void TestRoundTripStartingAtConfigWithAllFields()
+        {
+            var configIn = new JeUdpLogConfig
+            {
+                id = "test1",
+                Host = "logging.com",
+                LogBody = true,
+                MaxBodySize = 1234,
+                Port = 9999,
+                Timeout = 10000
+            };
+            var plugin = new JeUdpLogPlugin();
+            var configOut = PluginHelpers.RoundTripFromConfig(plugin, configIn);
+            configOut.IsExactMatch(configIn).Should().BeTrue();
+        }
+
+        [Fact]
+        public void RoundTripFromBodyWithNoData()
+        {
+            PluginHelpers.RoundTripFromBodyTest(new JeUdpLogPlugin());
+        }
+    }
+}
