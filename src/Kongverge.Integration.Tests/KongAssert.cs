236a5aaa42c633168448eaa8154040010aa1e34a
Anthony Steele <anthony.steele@just-eat.com>
2018-08-07 11:27:08 +0100

    Extract common code

--- src/Kongverge.Integration.Tests/KongAssert.cs
+++ src/Kongverge.Integration.Tests/KongAssert.cs
@@ -1,6 +1,7 @@
 using System.Linq;
 using System.Threading.Tasks;
 using FluentAssertions;
+using Kongverge.Common.DTOs;
 using Kongverge.Common.Services;
 
 namespace Kongverge.Integration.Tests
@@ -9,18 +10,20 @@ namespace Kongverge.Integration.Tests
     {
         public static async Task HasServiceWithId(this IKongAdminReader kongReader, string id)
         {
-            var allServices = await kongReader.GetServices();
-            var actual = allServices.FirstOrDefault(s => s.Id == id);
-
+            var actual = await GetServiceById(kongReader, id);
             actual.Should().NotBeNull($"Service with id '{id}' was not found");
         }
 
         public static async Task HasNoServiceWithId(this IKongAdminReader kongReader, string id)
         {
-            var allServices = await kongReader.GetServices();
-            var actual = allServices.FirstOrDefault(s => s.Id == id);
-
+            var actual = await GetServiceById(kongReader, id);
             actual.Should().BeNull($"Service with id '{id}' was found");
+        }
+
+        private static async Task<KongService> GetServiceById(IKongAdminReader kongReader, string id)
+        {
+            var allServices = await kongReader.GetServices();
+            return allServices.FirstOrDefault(s => s.Id == id);
         }
     }
 }
