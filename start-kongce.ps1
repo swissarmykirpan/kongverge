aae6f2646f6ab19fa9151f7a8acedb479a9cc7e8
Tim van Wijk <tim.vanwijk@just-eat.com>
2018-08-10 13:32:02 +0100

    Remove Casandra settings

--- start-kongce.ps1
+++ start-kongce.ps1
@@ -16,7 +16,7 @@ docker run -d --name kong-ce-database -p 5432:5432 -e "POSTGRES_USER=kong" -e "P
 Start-Sleep -s 10
 
 Write-Host "Running DB migrations"
-docker run --rm --link kong-ce-database:kong-ce-database -e "KONG_DATABASE=postgres" -e "KONG_PG_HOST=kong-ce-database" -e "KONG_CASSANDRA_CONTACT_POINTS=kong-ce-database" kong kong migrations up
+docker run --name kong-migrations --rm --link kong-ce-database:kong-ce-database -e "KONG_DATABASE=postgres" -e "KONG_PG_HOST=$kongdb_name"  kong kong migrations up
 
 
 $dockerOut = docker ps -a -f "ancestor=kong" | Select-String 'kong '
@@ -29,4 +29,4 @@ if ($dockerOut -match 'kong ') {
 }
 
 Write-Host "Starting Kong Server"
-docker run -d --name kong --link kong-ce-database:kong-ce-database -e "KONG_DATABASE=postgres" -e "KONG_PG_HOST=kong-ce-database" -e "KONG_CASSANDRA_CONTACT_POINTS=kong-ce-database" -e "KONG_PROXY_ACCESS_LOG=/dev/stdout" -e "KONG_ADMIN_ACCESS_LOG=/dev/stdout" -e "KONG_PROXY_ERROR_LOG=/dev/stderr" -e "KONG_ADMIN_ERROR_LOG=/dev/stderr" -e "KONG_VITALS=on" -e "KONG_ADMIN_LISTEN=0.0.0.0:8001" -e "KONG_PORTAL=on" -e "KONG_PORTAL_GUI_URI=localhost:8003" -p 8000:8000 -p 8443:8443 -p 8001:8001 -p 8444:8444 -p 8445:8445 kong
+docker run -d --name kong --link kong-ce-database:kong-ce-database -e "KONG_DATABASE=postgres" -e "KONG_PG_HOST=kong-ce-database" -e "KONG_PROXY_ACCESS_LOG=/dev/stdout" -e "KONG_ADMIN_ACCESS_LOG=/dev/stdout" -e "KONG_PROXY_ERROR_LOG=/dev/stderr" -e "KONG_ADMIN_ERROR_LOG=/dev/stderr" -e "KONG_ADMIN_LISTEN=0.0.0.0:8001" -p 8000:8000 -p 8443:8443 -p 8001:8001 -p 8444:8444 -p 8445:8445 kong
