b90ae493b86152dc20c4a022f1559afc6608548f
Anthony Steele <AnthonySteele@users.noreply.github.com>
2018-08-16 14:58:25 +0100

    Test a request transformer advance with all fields set at once (#88)

--- src/Kongverge.Integration.Tests/RequestTransformerPluginTests.cs
+++ src/Kongverge.Integration.Tests/RequestTransformerPluginTests.cs
@@ -116,5 +116,47 @@ namespace Kongverge.Integration.Tests
 
             await _fixture.ShouldRoundTripPlugInToKong(plugin);
         }
+
+        [Fact]
+        public async Task ServiceCanHaveRequestTransformerAdvancedPluginWithAllFieldsSet()
+        {
+            var plugin = new RequestTransformerAdvancedConfig
+            {
+                HttpMethod = "GET",
+                Add = new RequestTransformerAdvancedTransformBase
+                {
+                    Headers = new HashSet<string> { "aheader1:value1", "header2:Value2", "header3:value3" },
+                    QueryString = new HashSet<string> { "aparam_a:valueb", "param_b:value_d" },
+                    Body = new HashSet<string> { "akey1:value1" }
+                },
+                Append = new RequestTransformerAdvancedTransformBase
+                {
+                    Headers = new HashSet<string> { "bheader1:value1", "header2:Value2", "header3:value3" },
+                    QueryString = new HashSet<string> { "bparam_a:valueb", "param_b:value_d" },
+                    Body = new HashSet<string> { "bkey1:value1" }
+                },
+                Rename = new RequestTransformerAdvancedTransformBase
+                {
+                    Headers = new HashSet<string> { "cheader1:value1", "header2:Value2", "header3:value3" },
+                    QueryString = new HashSet<string> { "cparam_a:valueb", "param_b:value_d" },
+                    Body = new HashSet<string> { "ckey1:value1" }
+                },
+                Replace = new RequestTransformerAdvancedTransformReplace
+                {
+                    Uri = "/foo/bar",
+                    Headers = new HashSet<string> { "dheader1:value1", "header2:Value2", "header3:value3" },
+                    QueryString = new HashSet<string> { "dparam_a:valueb", "param_b:value_d" },
+                    Body = new HashSet<string> { "dkey1:value1" }
+                },
+                Remove = new RequestTransformerAdvancedTransformBase
+                {
+                    Headers = new HashSet<string> { "eheader1", "header2", "header3" },
+                    QueryString = new HashSet<string> { "eparam_a", "param_b" },
+                    Body = new HashSet<string> { "ekey1" }
+                }
+            };
+
+            await _fixture.ShouldRoundTripPlugInToKong(plugin);
+        }
     }
 }
