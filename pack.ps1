f32ba600d58509fd519439e5ebe6178566043244
Anthony Steele <anthony.steele@just-eat.com>
2018-08-28 17:05:44 +0100

    Caps

--- pack.ps1
+++ pack.ps1
@@ -8,15 +8,16 @@ param(
 
 . ./dotnetfunctions.ps1
 
+Remove-Item $OutputPath\*.*  -Force -Recurse
+
 Write-Host "Building solution..." -ForegroundColor Green
 & $dotnet build $solutionPath
 
 $fullVersion = $VersionPrefix + $VersionSuffix
 
-Write-Host "Packing KongVerge version $fullVersion" -ForegroundColor Green
-Remove-Item $OutputPath\*.*  -Force -Recurse
-& $dotnet pack .\src\Kongverge\KongVerge.csproj -o $OutputPath /p:PackageVersion=$fullVersion
+Write-Host "Packing Kongverge version $fullVersion" -ForegroundColor Green
+& $dotnet pack .\src\Kongverge\Kongverge.csproj -o $OutputPath /p:PackageVersion=$fullVersion
 
 $feedUrl = "http://packages.je-labs.com/nuget/Global/"
-Write-Host "Pushing KongVerge version $fullVersion to $feedUrl" -ForegroundColor Green
+Write-Host "Pushing Kongverge version $fullVersion to $feedUrl" -ForegroundColor Green
 & $dotnet nuget push $OutputPath\*.nupkg --source $feedUrl --api-key $NugetApiKey
