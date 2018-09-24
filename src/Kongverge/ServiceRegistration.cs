b50eee325fcbccb97ca6e05e82dcc3f3498e5a0d
Shayne van Asperen <shaynevanasperen@gmail.com>
2018-09-24 11:50:40 +0100

    Code cleanup

--- src/Kongverge/ServiceRegistration.cs
+++ src/Kongverge/ServiceRegistration.cs
@@ -18,7 +18,7 @@ namespace Kongverge
             Log.Information("Starting up");
         }
 
-        public static void AddServices(IServiceCollection services)
+        public static IServiceCollection ConfigureServices(this IServiceCollection services)
         {
             var configuration = new ConfigurationBuilder().AddEnvironmentVariables().Build();
             services.Configure<Settings>(x => configuration.Bind(x));
@@ -61,6 +61,8 @@ namespace Kongverge
                 Log.Information("Exporting information from Kong.");
                 return s.GetService<ExportWorkflow>();
             });
+
+            return services;
         }
     }
 }
