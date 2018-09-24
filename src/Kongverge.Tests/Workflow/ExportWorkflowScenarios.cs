38be720c1b8f1cd570c1fb6ca494e6abdcc7da94
Shayne van Asperen <shaynevanasperen@gmail.com>
2018-09-24 11:48:43 +0100

    Remove KongProcessor and test KongvergeWorkflow instead

--- src/Kongverge.Tests/Workflow/ExportWorkflowScenarios.cs
+++ src/Kongverge.Tests/Workflow/ExportWorkflowScenarios.cs
@@ -1,30 +1,13 @@
-using System.Threading.Tasks;
-using AutoFixture;
-using FluentAssertions;
-using Kongverge.DTOs;
 using Kongverge.Helpers;
 using Kongverge.Services;
 using Kongverge.Workflow;
-using Microsoft.Extensions.Options;
-using Moq;
 using TestStack.BDDfy;
 using TestStack.BDDfy.Xunit;
 
 namespace Kongverge.Tests.Workflow
 {
-    public class ExportWorkflowScenarios : ScenarioFor<ExportWorkflow>
+    public class ExportWorkflowScenarios : WorkflowSteps<ExportWorkflow>
     {
-        protected Settings Settings;
-        protected KongvergeConfiguration KongvergeConfiguration = new KongvergeConfiguration();
-        protected ExitCode ExitCode;
-
-        public ExportWorkflowScenarios()
-        {
-            Settings = new Fixture().Create<Settings>();
-            GetMock<IOptions<Settings>>().SetupGet(x => x.Value).Returns(Settings);
-            GetMock<ConfigBuilder>().Setup(x => x.FromKong(Get<IKongAdminReader>())).ReturnsAsync(KongvergeConfiguration);
-        }
-
         [BddfyFact(DisplayName = nameof(KongIsNotReachable))]
         public void Scenario1() =>
             this.Given(s => s.KongIsNotReachable())
@@ -41,16 +24,7 @@ namespace Kongverge.Tests.Workflow
                 .And(s => s.TheExitCodeIs(ExitCode.Success))
                 .BDDfy();
 
-        protected void KongIsNotReachable() => GetMock<IKongAdminReader>().Setup(x => x.KongIsReachable()).ReturnsAsync(false);
-
-        protected void KongIsReachable() => GetMock<IKongAdminReader>().Setup(x => x.KongIsReachable()).ReturnsAsync(true);
-
-        protected async Task Executing() => ExitCode = (ExitCode)await Subject.Execute();
-
-        protected void TheExitCodeIs(ExitCode exitCode) => ExitCode.Should().Be(exitCode);
-
-        protected void TheConfigurationIsRetrievedFromKong() => GetMock<ConfigBuilder>().Verify(x => x.FromKong(Get<IKongAdminReader>()));
-
-        protected void TheConfigurationIsWrittenToOutputFolder() => GetMock<ConfigFileWriter>().Verify(x => x.WriteConfiguration(KongvergeConfiguration, Settings.OutputFolder));
+        protected void TheConfigurationIsWrittenToOutputFolder() =>
+            GetMock<ConfigFileWriter>().Verify(x => x.WriteConfiguration(Existing, Settings.OutputFolder));
     }
 }
